70b6c9f98392568c0e4d4bfa52ca0c461002ff90
saddaul-siam <saddadul.siam@gmail.com>
2021-11-18 14:34:18 +0600

    added LOGIN  page

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,44 @@
+import { Button, Container, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PrimaryButton from '../../../CoustomStyle/MuiButton';
+import Navigation from '../../Shared/Navigation/Navigation';
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <Box>
+      <Navigation />
+      <Container sx={{ width: '570px', border: '1px solid black', mt: 15 }} >
+        <Box sx={{ display: 'flex', justifyContent: 'center', }}>
+          <Box>
+            <Typography sx={{ fontSize: 24, fontWeight: 700, pt: 5 }}>
+              Login With
+            </Typography>
+            <TextField sx={{ width: "100%", my: 2 }} label="Email" variant="standard" />
+            <TextField sx={{ width: "100%", my: 2 }} label="Password" variant="standard" />
+
+            <PrimaryButton variant="contained" sx={{ width: "100%", my: 3 }} >Login</PrimaryButton>
+
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Typography sx={{ fontSize: "16px", pb: 3 }}>
+                New user? <Link style={{ color: '#F63E7B' }} to="/register">register</Link>
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+      <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+        <Typography sx={{ pb: 3 }}>
+          ----------------------------------------or----------------------------------------
+        </Typography>
+        <Button variant="outlined"><FacebookIcon />  Continue with Facebook</Button>
+        <br />
+        <Button variant="outlined"><GoogleIcon />  Continue with Google</Button>
+      </Box>
+    </Box>
   );
 };
 
